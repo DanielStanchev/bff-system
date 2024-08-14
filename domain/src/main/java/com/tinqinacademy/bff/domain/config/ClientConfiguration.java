a397ea32fd26421f82cffb069e3eaffd9377921c
Daniel Stanchev <danyto0o@mail.bg>
2024-08-14 23:38:19 +0300

    t12 - fix UpdateRoomPartially to properly handle output result and add Input fields to be able to be null.Add Feign okhttp client enabling patch requests

--- domain/src/main/java/com/tinqinacademy/bff/domain/config/ClientConfiguration.java
+++ domain/src/main/java/com/tinqinacademy/bff/domain/config/ClientConfiguration.java
@@ -1,14 +1,17 @@
 package com.tinqinacademy.bff.domain.config;
 
+
+import feign.okhttp.OkHttpClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientConfiguration {
-    //used with feign client
-//    @Bean
-//    public OkHttpClient client() {
-//        return new OkHttpClient();
-//    }
+    //used with feign client as default http client used by feign does not support PATCH requests
+    @Bean
+    public OkHttpClient client() {
+        return new OkHttpClient();
+    }
 
 ////used with requestLine
 //    @Bean
