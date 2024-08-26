95063d4107777c6e3c24aa34829a13e3f2841cd7
Daniel Stanchev <148866644+DanielStanchev@users.noreply.github.com>
2024-08-26 13:22:29 +0300

    t16 - add cors disable in spring security config due to all endpoints forbidden

--- core/src/main/java/com/tinqinacademy/bff/core/config/securityconfig/SecurityConfig.java
+++ core/src/main/java/com/tinqinacademy/bff/core/config/securityconfig/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(request -> {
                 adminRequestsAuthorization(request);
                 userRequestsAuthorization(request);
